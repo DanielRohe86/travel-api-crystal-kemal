27fb512242ac4a2e68a9567c35bffcf6c335da00
DanielRohe86 <psicologo.rohe@gmail.com>
2023-06-22 08:31:25 -0300

    [fix]dockerfile works with GET method, method still not working on Thunderclient

--- src/travel_api_integration.cr
+++ src/travel_api_integration.cr
@@ -1,46 +1,63 @@
-require "http/client"
-require "json"
+# require "json"
+# require "kemal"
 
+# base_url = "https://rickandmortyapi.com/api"
 
-base_url = "http://localhost:3000"
+# def create_travel_plan(travel_stops : Array(Int32)) : Hash(String, JSON::Any)? 
+#   url = "#{base_url}/travel-plans"
+#   payload = travel_stops.to_json
+#   response = HTTP::Client.post(url, body: payload, headers: {"Content-Type" => "application/json"})
 
+#   if response.status_code == 201
+#     puts "Travel plan created successfully!"
+#     JSON.parse(response.body).as(Hash(String, JSON::Any))
+#   else
+#     puts "Failed to create travel plan."
+#     nil
+#   end
+# end
 
-def create_travel_plan(travel_stops : Array(Int32)) : Hash(String, JSON::Any)? 
-  url = "#{base_url}/travel-plans"
-  payload = travel_stops.to_json
-  response = HTTP::Client.post(url, body: payload, headers: {"Content-Type" => "application/json"})
+# def get_travel_plans : Array(Hash(String, JSON::Any))?
+#   url = "#{base_url}/travel-plans"
+#   response = HTTP::Client.get(url)
 
-  if response.status_code == 201
-    puts "Travel plan created successfully!"
-    JSON.parse(response.body).as(Hash(String, JSON::Any))
-  else
-    puts "Failed to create travel plan."
-    nil
-  end
-end
+#   if response.status_code == 200
+#     JSON.parse(response.body).as(Array(Hash(String, JSON::Any)))
+#   else
+#     puts "Failed to retrieve travel plans."
+#     nil
+#   end
+# end
 
+# get "/travel-plans" do |env|
+#   env.response.content_type = "application/json"
+#   env.response.status_code = 200
+#   travel_plans.to_json
+# end
 
-def get_travel_plans : Array(Hash(String, JSON::Any))? 
-  url = "#{base_url}/travel-plans"
-  response = HTTP::Client.get(url)
+# post "/travel-plans" do |env|
+#   env.response.content_type = "application/json"
+#   env.response.status_code = 201
 
-  if response.status_code == 200
-    JSON.parse(response.body).as(Array(Hash(String, JSON::Any)))
-  else
-    puts "Failed to retrieve travel plans."
-    nil
-  end
-end
+#   travel_stops = JSON.parse(env.request.body.to_s)
+#   travel_plan = Hash(String, JSON::Any).new
+#   travel_plan["id"] = JSON::Any.new((travel_plans.size + 1).to_s)
+#   travel_plan["travel_stops"] = travel_stops
+#   travel_plans << travel_plan
 
+#   travel_plan.to_json
+# end
 
-created_plan = create_travel_plan([1, 2])
-if created_plan
-  puts "Created Travel Plan:"
-  puts JSON.pretty_generate(created_plan)
-end
+# Kemal.run
 
-all_plans = get_travel_plans
-if all_plans
-  puts "All Travel Plans:"
-  puts JSON.pretty_generate(all_plans)
-end
+# created_plan = create_travel_plan([1, 2])
+# if created_plan
+#   puts "Created Travel Plan:"
+#   puts JSON.pretty_generate(created_plan)
+# end
+
+# all_plans = get_travel_plans
+# if all_plans
+#   puts "All Travel Plans:"
+#   puts JSON.pretty_generate(all_plans)
+# end
--- src/travel_plans.cr
+++ src/travel_plans.cr
@@ -1,32 +1,66 @@
+require "json"
 require "kemal"
+require "http/client"
 
-# In-memory storage for travel plans
 travel_plans = [] of Hash(String, JSON::Any)
 
-# GET /travel-plans
+# def create_travel_plan(base_url : String, travel_stops : Array(Int32)) : JSON::Any? 
+#   url = "#{base_url}/travel-plans"
+#   payload = travel_stops.to_json
+#   response = HTTP::Client.post(url, body: payload, headers: HTTP::Headers{"Content-Type" => "application/json"})
+
+#   if response.status_code == 201
+#     puts "Travel plan created successfully!"
+#     JSON.parse(response.body.to_s)
+#   else
+#     puts "Failed to create travel plan."
+#     nil
+#   end
+# end
+
+def get_travel_plans(base_url : String) : Array(String)?
+  url = "#{base_url}/travel-plans"
+  response = HTTP::Client.get(url)
+  
+  if response.status_code == 200
+    puts response.to_s
+    JSON.parse(response.body.to_s).as(Array(String))
+  else
+    puts "Failed to retrieve travel plans."
+    nil
+  end
+end
+
+# post "/travel-plans" do |env|
+#   env.response.content_type = "application/json"
+#   env.response.status_code = 201
+
+#   travel_stops = JSON.parse(env.request.body.to_s)
+#   travel_plan = {"id" => (travel_plans.size + 1).to_s, "travel_stops" => travel_stops}
+#   travel_plans << travel_plan
+
+#   travel_plan.to_json
+# end
+
 get "/travel-plans" do |env|
   env.response.content_type = "application/json"
   env.response.status_code = 200
   travel_plans.to_json
 end
 
-# POST /travel-plans
-post "/travel-plans" do |env|
-  env.response.content_type = "application/json"
-  env.response.status_code = 201
 
-  # Retrieve the travel stops from the request body
-  travel_stops = JSON.parse(env.request.body.to_s)
+Kemal.run do
+  base_url = "https://rickandmortyapi.com/api"
 
-  # Create a new travel plan
-  travel_plan = Hash(String, JSON::Any).new
-  travel_plan["id"] = JSON::Any.new((travel_plans.size + 1).to_s)
-  travel_plan["travel_stops"] = travel_stops
+  # created_plan = create_travel_plan(base_url, [1, 2])
+  # if created_plan
+  #   puts "Created Travel Plan:"
+  #   puts created_plan.to_pretty_json
+  # end
 
-  # Add the travel plan to the storage
-  travel_plans << travel_plan
-
-  travel_plan.to_json
+  all_plans = get_travel_plans(base_url)
+  if all_plans
+    puts "All Travel Plans:"
+    puts all_plans.to_pretty_json
+  end
 end
-
-Kemal.run
