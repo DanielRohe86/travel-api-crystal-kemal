5a5f99a16d773ec675f594c76d98bb706bd9274b
DanielRohe86 <psicologo.rohe@gmail.com>
2023-06-24 14:57:36 -0300

    [feat] adiciona método get, funciona com teste no postman após passar alguns POST

--- src/travel_plans_crud.cr
+++ src/travel_plans_crud.cr
@@ -1,5 +1,6 @@
 require "kemal"
 # require "json"
+# require "./travel_plans_fetch.cr"
 
 module Api
   VERSION = "0.1.0"
@@ -15,12 +16,21 @@ module Api
     travel_stops = context.params.json["travel_stops"]
     travel_plan = %({"id": #{id}, "travel_stops": #{travel_stops}})   
 
+    context.response.headers["Content-Type"] = "application/json"
     context.response.status_code = 201
     @@travel_plans << travel_plan
     json_array = "[" + @@travel_plans.join(",") + "]"
 
     json_array
   end
+
+  get "/travel-plans" do |context|
+    context.response.headers["Content-Type"] = "application/json"
+    context.response.status_code = 200
+
+    @@travel_plans.to_json
+  end
+
 end
 
 Kemal.run
