119e320c10091ae836662f8535cbc046b568cf9c
DanielRohe86 <psicologo.rohe@gmail.com>
2023-06-24 14:48:35 -0300

    [fix] finaliza método post sem sobrescrever a cada chamada

--- src/travel_plans_crud.cr
+++ src/travel_plans_crud.cr
@@ -4,19 +4,22 @@ require "kemal"
 module Api
   VERSION = "0.1.0"
 
+  @@travel_plans = [] of String
   @@last_id = 0
  
   post "/travel-plans/" do |context|
     # id = context.params.url["id"] source for id retrieved from /:id
     @@last_id += 1
-    id = @@last_id.to_s
+    id = @@last_id
 
     travel_stops = context.params.json["travel_stops"]
-    context.response.status_code = 201    
-    "{
-      id: #{id},
-    travel_stops: #{travel_stops}
-    }"    
+    travel_plan = %({"id": #{id}, "travel_stops": #{travel_stops}})   
+
+    context.response.status_code = 201
+    @@travel_plans << travel_plan
+    json_array = "[" + @@travel_plans.join(",") + "]"
+
+    json_array
   end
 end
 
