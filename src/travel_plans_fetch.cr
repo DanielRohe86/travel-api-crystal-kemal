fae282aa94c9d0c8d19c1128d256c6ebed1b330b
DanielRohe86 <psicologo.rohe@gmail.com>
2023-06-25 08:58:43 -0300

    [feat] loop sobre a API retornando planeta, tipo e dimensão - valor 0 como teste para retornar o equivalente ao id=1 no método GET com expand=true

--- src/travel_plans_fetch.cr
+++ src/travel_plans_fetch.cr
@@ -1,32 +1,49 @@
 require "json"
 require "kemal"
 
-# fetch api
-def get_travel_plans(base_url : String) : Array(String)?
+# Fetch API and retrieve travel plans
+def get_travel_plans(base_url : String) : {Array(String), Array(String), Array(String)}?
   url = "#{base_url}/location"
   response = HTTP::Client.get(url)
   planets = [] of String
+  types = [] of String
+  dimensions = [] of String
 
   if response.status_code == 200
     json = JSON.parse(response.body.to_s)
 
     iterator = 0
     while iterator <= 19
-      results = json["results"][iterator]["name"].to_s
-      planets << results
+      results_planets = json["results"][iterator]["name"].to_s
+      results_types = json["results"][iterator]["type"].to_s
+      results_dimensions = json["results"][iterator]["dimension"].to_s
+
+      planets << results_planets
+      types << results_types
+      dimensions << results_dimensions
+
       iterator += 1
     end
 
-    planets
+    {planets, types, dimensions}
   else
     puts "Failed to retrieve travel plans."
-    nil
+    return nil
   end
 end
 
 base_url = "https://rickandmortyapi.com/api"
 all_plans = get_travel_plans(base_url)
-puts all_plans
+
+if all_plans.nil?
+  puts "No travel plans available."
+else
+  planets, types, dimensions = all_plans
+
+  puts "Planets: #{planets[0]}"
+  puts "Types: #{types[0]}"
+  puts "Dimensions: #{dimensions[0]}"
+end
 
 Kemal.run
 
